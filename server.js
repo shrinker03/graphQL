cf11c19c7ee1c6735fcf690993d0b0a45fc3c0b0
DESKTOP-65CV4VO\Acer <shivamdamre913@gmail.com>
2025-12-11 21:07:03 +0530

    Added express and started with module 2

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-const { buildSchema, graphql } = require("graphql");
+const express = require("express");
+const { buildSchema } = require("graphql");
+// FIX: Import from the specific Express adapter
+const { createHandler } = require("graphql-http/lib/use/express");
+const { ruruHTML } = require("ruru/server");
 
 const schema = buildSchema(`type Query { hello: String }`);
 
 const rootValue = {
-    hello() {
-        return 'Hello World!';
-    },
+  hello() {
+    return 'Hello World!';
+  },
 }
 
-graphql({
+const app = express();
+
+// Serve the Ruru (GraphiQL) UI at the root
+app.get('/', (_req, res) => {
+  res.type('html');
+  res.end(ruruHTML({ endpoint: '/graphql' }));
+});
+
+// Serve the actual API logic at /graphql
+app.all('/graphql', createHandler({
     schema,
-    source: '{ hello }',
-    rootValue,
-}).then((response) => {
-    console.log(response);
-})
+    rootValue
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
